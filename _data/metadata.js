d9737c245fb13cb2c4c21680414afb1573b06ab5
Nami Sunami <nsunami@pm.me>
2024-09-21 20:58:13 +0200

    Update metadata

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Nami Writes - Nami's Blog",
+	url: "https://blog.namisunami.com/",
+	language: "en",
+	description: "Nami writes about things",
+	author: {
+		name: "Nami Sunami",
+		email: "nsunami@pm.me",
+		url: "https://blog.namisunami.com/about-me/",
+	},
+};
